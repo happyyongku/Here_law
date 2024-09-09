70803218ffc47edcfa5c4e952317d1a900c29875
DongJun <djleeasi@gmail.com>
2024-09-09 12:46:57 +0900

    spring initial commit

---
+++ backend/spring/src/main/java/org/ssafyb109/here_law/HereLawApplication.java
@@ -0,0 +1,13 @@
+package org.ssafyb109.here_law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereLawApplication.class, args);
+	}
+
+}
---
+++ backend/spring/src/test/java/org/ssafyb109/here_law/HereLawApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafyb109.here_law;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereLawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
