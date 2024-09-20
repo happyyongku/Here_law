99995a8afdf89a206271d441c83cacfb695ef019
kimhaesu <gotnsla12@naver.com>
2024-09-20 13:32:30 +0900

    Feat: 일반유 관심분야 컴포넌트 구현

--- frontend/here_law/src/components/mypage/Interest.jsx
+++ frontend/here_law/src/components/mypage/Interest.jsx
@@ -1,8 +1,25 @@
+import InterestCard from "./InterestCard";
+import addinterestimg from "../../assets/mypage/addexpertise.png";
+import "./Interest.css";
+
 function Interest() {
+  const interest = ["부동산", "형사", "노동"];
   return (
-    <>
-      <div>관심분야입니다.</div>
-    </>
+    <div className="interest-container">
+      <div className="interest-header">
+        <h3 className="interest-header-title">관심분야</h3>
+        <img
+          className="add-interest"
+          src={addinterestimg}
+          alt="addinterestimg"
+        />
+      </div>
+      <div className="interest-items">
+        {interest.map((item, index) => (
+          <InterestCard key={index} item={item} />
+        ))}
+      </div>
+    </div>
   );
 }
 
