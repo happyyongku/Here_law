048970ad00524c9aded07e687146a30d5c031b45
happyyongku <yonggu1951@gmail.com>
2024-09-23 13:57:32 +0900

    Test: Build test

---
+++ DB/init.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.user_entity (created_date,email,email_token,is_email_verified,is_first,nickname,"password",profile_img,update_date,user_type) VALUES
+	 ('2024-09-20 16:06:40.87377','ongyeom14@naver.com',NULL,true,true,'user1','$2a$10$AJrG.qTNfpDyWlw8HRLB1eFnu.XP1UoMOaAvl0J8UdlTVm6PW.dS6','base64String','2024-09-20 16:06:40.87377','normal'),
+	 ('2024-09-20 16:09:19.376496','ggs0019@naver.com',NULL,true,true,'user2','$2a$10$BSZZvF1txUxZOJQCBWV7NecQuY84irU5Ko7etUtoTS36i4PrWLjJ6','https://example.com/profile.jpg','2024-09-20 16:09:19.376496','lawyer');
