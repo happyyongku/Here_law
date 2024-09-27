cc9faa421419f19858704514fa7a69acffb9969d
전온겸 <ongyeom14@naver.com>
2024-09-27 12:52:23 +0900

    Fix: 단순검색 수정

---
+++ backend/spring/src/main/java/org/ssafyb109/here_law/repository/jpa/CaseJpaRepository.java
@@ -0,0 +1,10 @@
+package org.ssafyb109.here_law.repository.jpa;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.ssafyb109.here_law.entity.CaseEntity;
+
+public interface CaseJpaRepository extends JpaRepository<CaseEntity, Long> {
+    Page<CaseEntity> findBySummaryContaining(String keyword, Pageable pageable);
+}
