72462b7213732a9bbae8d65717f345447e782e6e
happyyongku <yonggu1951@gmail.com>
2024-10-04 17:57:24 +0900

    Feat: 페이지 마운트 시 API 수정 - 진행 중

--- backend/fastapi_ec2/router/magazine_router.py
+++ backend/fastapi_ec2/router/magazine_router.py
@@ -1,13 +1,13 @@
 from http import server
 from turtle import st
-from fastapi import APIRouter, HTTPException, Depends, status, Request
+from fastapi import APIRouter, HTTPException, Depends, status, Request, Response
+from fastapi.responses import JSONResponse
 from utils.security import get_current_user
 from pydantic import BaseModel
-from sqlalchemy import BIGINT, VARCHAR, BigInteger, Boolean, ForeignKey, create_engine, Column, Integer, String, Text, TIMESTAMP, null, ForeignKey
-from sqlalchemy.ext. declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, relationship
-from sqlalchemy.sql import func
 from dotenv import load_dotenv
+from dto.user_model import UserEntity, UserInterests, Magazine, UserSubscription
 import requests
 import os
 import logging
@@ -30,7 +30,8 @@ print(f"DATABASE_URL={DATABASE_URL}")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+# Base = declarative_base()
+# ------------------------------------------------변수 영역-----------------------------------------
 
 def get_db():
     db = SessionLocal()
@@ -39,60 +40,57 @@ def get_db():
     finally:
         db.close()
         
-class UserEntity(Base):
-    __tablename__="user_entity"
-    id = Column(BigInteger, primary_key=True, index=True)
-    created_date = Column(TIMESTAMP, server_default=func.now(), nullable=False)
-    email = Column(String(255), nullable=True)
-    email_token=Column(String(255), nullable=True)
-    is_email_verified = Column(Boolean, nullable=True)
-    is_first=Column(Boolean, nullable=True)
-    nickname=Column(String(255), nullable=True)
-    password=Column(String(255), nullable=True)
-    phone_number=Column(String(255), nullable=True)
-    update_date=Column(TIMESTAMP, server_default=func.now(), nullable=True)
-    user_type=Column(String(255), nullable=True)
+def interest_query(user, db):
+    try:
+        user_intersts = db.query(UserInterests).filter(UserInterests.user_entity_id == user.id).all()
+        interest_categories = [interest.interests for interest in user_intersts]
+        magazines = (db.query(Magazine)
+                    .filter(Magazine.category.in_(interest_categories))
+                    .order_by(Magazine.created_at.desc())
+                    .limit(5)
+                    .all())
+        if not magazines:
+            print("No interest Magazine")
+            magazines = db.query(Magazine).order_by(Magazine.created_at.desc()).limit(5).all()
+    except:
+        print("Error in interest")
+        magazines = db.query(Magazine).order_by(Magazine.created_at.desc()).limit(5).all()
     
-    interests = relationship("UserInterests", back_populates="user")
+    return [mag.as_dict() for mag in magazines]
 
-class UserInterests(Base):
-    __tablename__="user_entity_interests"
-    user_entity_id = Column(BigInteger, ForeignKey("user_entity.id"), nullable=False, primary_key=True)
-    interests = Column(String(255))
-    user = relationship("UserEntity", back_populates="interests")
-        
-class Magazine(Base):
-    __tablename__ = "magazines"
-    magazine_id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), nullable=False)
-    category = Column(String(100), nullable=False)
-    created_at = Column(TIMESTAMP, server_default=func.now(), nullable=False)
-    image = Column(String(255), default='default.jpg')
-    content = Column(Text, nullable=False)
-    view_count = Column(Integer, default=0)
-    likes = Column(Integer, default=0)
+def subscriptions_query(user, db):
+    try:
+        user_subscriptions = db.query(UserSubscription).filter(UserSubscription.user_id == user.id).all()
+        subscription_category = [subscription.subscriptions for subscription in user_subscriptions]
+        magazines = (db.query(Magazine)
+                    .filter(Magazine.category.in_(subscription_category))
+                    .order_by(Magazine.created_at)
+                    .limit(5)
+                    .all())
+        if not magazines:
+            print("No subscription Magazine")
+            magazines = db.query(Magazine).order_by(Magazine.created_at.desc()).limit(5).all()
+    except:
+        print("Error in Subscription")
+        magazines = db.query(Magazine).order_by(Magazine.created_at.desc()).limit(5).all()
     
+    return [mag.as_dict() for mag in magazines]
 
+# -----------------------------------------------함수 영역 ------------------------------------------------
 
 @router.get("/")
-def magazine_mount(request:Request, token:str=Depends(get_current_user), db: Session=Depends(get_db)):
+def magazine_mount_interest(token:str=Depends(get_current_user), db: Session=Depends(get_db)):
     user_email = str(token).split("'")[1]
     user = db.query(UserEntity).filter(UserEntity.email == user_email).first()
-    print("///////////////////////////////")
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
-    user_intersts = db.query(UserInterests).filter(UserInterests.user_entity_id == user.id).all()
-    interest_categories = [interest.interests for interest in user_intersts]
-    if not interest_categories:
-        raise HTTPException(status_code=404, detail="Interest not found")
+    interest_magazines = interest_query(user, db)
+    subscription_magazines = subscriptions_query(user, db)
     
-    magazines = (db.query(Magazine)
-                .filter(Magazine.category.in_(interest_categories))
-                .order_by(Magazine.created_at.desc())
-                .limit(5)
-                .all())
-    if not magazines:
-        magazines = db.query(Magazine).order_by(Magazine.created_at.desc()).limit(5).all() 
-
-    return magazines
+    magazines_obj = {
+        'interest': interest_magazines,
+        'subscription': subscription_magazines
+    }
+    
+    return JSONResponse(content=magazines_obj)
