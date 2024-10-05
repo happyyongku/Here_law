e1a6dc385fc00d86f4aba07d424a4ed75b8f7a16
qudrb0107 <qudrb0107@naver.com>
2024-10-05 05:26:27 +0000

    충돌좀 그만나~~~

--- backend/fastapi_ec2/main.py
+++ backend/fastapi_ec2/main.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
 from fastapi import FastAPI
-from router.chat_router import chat_router
+# from router.chat_router import chat_router
 from router.clause_router import clause_analysis_router
 from router.sojang_router import sojang_router
 from router.shearch_router import case_router
@@ -15,7 +15,7 @@ app = FastAPI()
 
 FASTAPI_SERVER_PORT = int(os.environ.get("FASTAPI_SERVER_PORT"))
 
-app.include_router(chat_router, prefix="/fastapi_ec2/chat")
+# app.include_router(chat_router, prefix="/fastapi_ec2/chat")
 app.include_router(clause_analysis_router, prefix="/fastapi_ec2/clause")
 app.include_router(sojang_router, prefix="/fastapi_ec2/sojang")
 app.include_router(case_router, prefix="/fastapi_ec2/case")
