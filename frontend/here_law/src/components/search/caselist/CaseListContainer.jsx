318de005df1d5fbc52e77687832b2f18e11eb061
Kang Kyung Min <ski03259@naver.com>
2024-10-07 01:10:38 +0900

    Feat: 로딩 기능 추가

--- frontend/here_law/src/components/search/caselist/CaseListContainer.jsx
+++ frontend/here_law/src/components/search/caselist/CaseListContainer.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 
-// 리덕스로 불러오기
-import { useDispatch, useSelector } from "react-redux";
-import { loadCases } from "../../../redux/actions/caseActions";
-
-// axios로 불러오기
 import axiosInstance from "../../../utils/axiosInstance";
 
 import SortIcon from "../../../assets/search/sorticon.png";
@@ -15,31 +10,14 @@ import CaseItem from "./CaseItem";
 import Page from "./Page";
 import "./CaseListContainer.css";
 
+import Loader from "./Loader";
+
 const CaseListContainer = () => {
   const [searchParams] = useSearchParams();
-
-  const query = searchParams.get("query") || ""; // 쿼리 파라미터 읽기
-
+  const query = searchParams.get("query") || ""; // 쿼리(keyword)
   // console.log(query);
-
-  const page = Number(searchParams.get("page")) || 1; // 페이지 파라미터 읽기
-
+  const page = Number(searchParams.get("page")) || 1; // 페이지
   // console.log(page);
-
-  // 리덕스
-  // const dispatch = useDispatch(); // 액션 디스패치 함수
-  // const caseData = useSelector((state) => state.cases); // 리덕스 스토어에서 가져오기
-
-  // console.log(caseData);
-  // console.log("1번 강경민");
-
-  // // page가 변경될 때마다 데이터 요청
-  // useEffect(() => {
-  //   dispatch(loadCases(query, page));
-  // }, [query, page, dispatch]);
-
-  // console.log(caseData);
-  // console.log("2번 강경민");
 
   const [caseData, setCaseData] = useState({
     cases: [],
@@ -47,10 +25,12 @@ const CaseListContainer = () => {
     totalPages: 0,
   });
 
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태 추가
+
   // 판례 상세 조회 axios 요청
   const CaseSearchRequest = async () => {
     const token = localStorage.getItem("token");
-    console.log(token);
+    setIsLoading(true); // 로딩 시작
     try {
       const response = await axiosInstance.get(`/spring_api/cases/search`, {
         params: {
@@ -65,6 +45,7 @@ const CaseListContainer = () => {
     } catch (error) {
       console.log("판례검색 실패", error);
     }
+    setIsLoading(false); // 로딩 종료
   };
 
   useEffect(() => {
@@ -90,49 +71,65 @@ const CaseListContainer = () => {
     <div className="case-list-container">
       <CaseHeader query={query} />
 
-      <div className="case-list-page">
-        <div className="case-list-top">
-          전문 판례{" "}
-          <span style={{ fontWeight: "bold" }}>{caseData.totalResults}개</span>
-          가 검색되었습니다.
-          <div
-            className={`case-list-sort ${
-              selectedCaseType !== "사건 종류" ? "selected" : ""
-            }`}
-            onClick={toggleCaseType}
-          >
-            {selectedCaseType}
-            <img
-              src={selectedCaseType !== "사건 종류" ? SortIcon2 : SortIcon}
-              alt="sort icon"
-              className="sort-icon"
-            />
-          </div>
-        </div>
+      {/* 로딩 중일 때 로딩 컴포넌트 표시 */}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="case-list-page">
+            <div className="case-list-top">
+              {/* 검색 결과가 있을 때만 표시 */}
+              {caseData.totalResults > 0 && (
+                <>
+                  전문 판례{" "}
+                  <span style={{ fontWeight: "bold" }}>
+                    {caseData.totalResults}개
+                  </span>
+                  가 검색되었습니다.
+                </>
+              )}
 
-        {/* 사건 종류 리스트 토글 */}
-        {isCaseTypeVisible && (
-          <div className="case-type-list">
-            {caseTypes.map((type) => (
-              <div key={type} onClick={() => handleCaseTypeClick(type)}>
-                {type}
+              <div
+                className={`case-list-sort ${
+                  selectedCaseType !== "사건 종류" ? "selected" : ""
+                }`}
+                onClick={toggleCaseType}
+              >
+                {selectedCaseType}
+                <img
+                  src={selectedCaseType !== "사건 종류" ? SortIcon2 : SortIcon}
+                  alt="sort icon"
+                  className="sort-icon"
+                />
               </div>
-            ))}
-            {/* 추가 사건 종류 */}
+            </div>
+
+            {/* 사건 종류 리스트 토글 */}
+            {isCaseTypeVisible && (
+              <div className="case-type-list">
+                {caseTypes.map((type) => (
+                  <div key={type} onClick={() => handleCaseTypeClick(type)}>
+                    {type}
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {/* 검색 결과가 없을 때 메시지 표시 */}
+            {
+              caseData.cases.length > 0
+                ? caseData.cases.map((caseItem) => (
+                    <CaseItem key={caseItem.caseInfoId} caseItem={caseItem} />
+                  ))
+                : query && (
+                    <p>검색 결과가 없습니다.</p>
+                  ) /* 검색 쿼리가 있을 때만 결과 없음 표시 */
+            }
+
+            <Page query={query} page={page} totalPages={caseData.totalPages} />
           </div>
-        )}
-
-        {/* 판례 리스트 */}
-        {caseData.cases.length > 0 ? (
-          caseData.cases.map((caseItem) => (
-            <CaseItem key={caseItem.caseInfoId} caseItem={caseItem} />
-          ))
-        ) : (
-          <p>검색 결과가 없습니다.</p>
-        )}
-
-        <Page query={query} page={page} totalPages={caseData.totalPages} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
