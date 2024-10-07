d7f7a5d1e463fcea412880133b518e2f93f03359
강병규 <qudrb0107@naver.com>
2024-10-07 10:38:21 +0900

    Update news_router.py

--- backend/fastapi_ec2/router/news_router.py
+++ backend/fastapi_ec2/router/news_router.py
@@ -35,10 +35,10 @@ class News(BaseModel):
 
 # 1. 모든 뉴스 조회 API (조회수 증가 없음)
 @news_router.get("/news/", response_model=List[News])
-def get_all_news(skip: int = 0, limit: int = 10, db: psycopg2.extensions.connection = Depends(get_db)):
+def get_all_news(db: psycopg2.extensions.connection = Depends(get_db)):
     cursor = db.cursor()
     query = "SELECT * FROM news ORDER BY created_at DESC;"
-    cursor.execute(query, (skip, limit))
+    cursor.execute(query)
     news_list = cursor.fetchall()
     return news_list
 
