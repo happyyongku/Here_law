744a4bbf954258a1f2d75924c1733756a4d39134
unknown <ski03259@naver.com>
2024-10-07 12:41:07 +0900

    Feat:L

--- frontend/here_law/src/utils/fastaxiosInstance.js
+++ frontend/here_law/src/utils/fastaxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fastaxiosInstance = axios.create({
-  baseURL: "https://3.36.85.129", // FastAPI 서버의 baseURL 설정
+  baseURL: "https://3.36.85.129:8000", // FastAPI 서버의 baseURL 설정
   withCredentials: true, // 필요한 경우 쿠키를 함께 전송할 수 있도록 설정
 });
 
