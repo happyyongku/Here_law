58dad04cee4edcd70dc4868799a64be2dfef4c8c
qudrb0107 <qudrb0107@naver.com>
2024-10-07 01:11:01 +0000

    Your commit message

--- backend/fastapi_ec2/main.py
+++ backend/fastapi_ec2/main.py
@@ -8,6 +8,7 @@ from router.search_router import case_router
 from router.bill_router import bill_router
 from router.magazine_router import router as magazine_router
 from router.get_newbill_router import new_bill_router
+from router.news_router import news_router
 
 import logging
 
@@ -20,6 +21,7 @@ app.include_router(case_router, prefix="/fastapi_ec2/case")
 app.include_router(magazine_router, prefix="/fastapi_ec2/magazine")
 app.include_router(bill_router, prefix='/fastapi_ec2/bill')
 app.include_router(new_bill_router, prefix='/fastapi_ec2/new_bill')
+app.include_router(news_router, prefix='/fastapi_ec2/news')
 
 @app.get('/fastapi_ec2/')
 def test():
