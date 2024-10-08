9c0974f919512716534af87b25cd6560af41f8f7
unknown <ski03259@naver.com>
2024-10-08 17:41:26 +0900

    Feat: 소장 작성 인풋 페이지

--- frontend/here_law/src/routes/AppRoutes.jsx
+++ frontend/here_law/src/routes/AppRoutes.jsx
@@ -22,6 +22,8 @@ import RentDocument from "../pages/RentDocument";
 import DocumentUpload from "../pages/DocumentUpload";
 import DocumentResult from "../pages/DocumentResult";
 
+import SojangInput from "../pages/SojangInput";
+
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token");
   console.log(token);
@@ -55,6 +57,7 @@ function AppRoutes() {
 
       <Route path="/document/upload" element={<DocumentUpload />} />
       <Route path="/document/result" element={<DocumentResult />} />
+      <Route path="/sojang/input" element={<SojangInput />} />
 
       <Route path="/magazine" element={<Magazine />}>
         <Route path="" element={<MagazineMain />}></Route>
