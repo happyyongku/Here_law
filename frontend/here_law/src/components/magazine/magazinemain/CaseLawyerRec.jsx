06938dbcc173729ec797b1cab8451074654e34d3
kimhaesu <gotnsla12@naver.com>
2024-10-11 06:29:11 +0900

    사진이어받기

--- frontend/here_law/src/components/magazine/magazinemain/CaseLawyerRec.jsx
+++ frontend/here_law/src/components/magazine/magazinemain/CaseLawyerRec.jsx
@@ -1,13 +1,22 @@
+import lawyer from "../../../assets/lawyer/lawyer.png";
+import lawyer1 from "../../../assets/lawyer/lawyer1.jfif";
+import lawyer2 from "../../../assets/lawyer/lawyer2.jpg";
+import lawyer3 from "../../../assets/lawyer/lawyer3.jpg";
+import lawyer4 from "../../../assets/lawyer/lawyer4.jpg";
+
 import LawyerDetail from "./LawyerDetail";
 import "./CaseLawyerRec.css";
 
-function CaseLawyerRec({ item, onClick, setLawyerId }) {
-  console.log(item.lawyer_id);
+function CaseLawyerRec({ item, onClick, setLawyerId, index }) {
+  // console.log(item.lawyer_id);
+  const lawyerArray = [lawyer, lawyer1, lawyer2, lawyer3, lawyer4];
   return (
     <div className="lawyer-rec-container">
-      {/* <img src="" alt="" /> */}
-      {/* 이미지 대용 임시 div 태그 */}
-      <div className="lawyer-rec-img"></div>
+      <img
+        src={lawyerArray[index]}
+        alt="lawyerimg"
+        className="lawyer-rec-img"
+      />
       <div
         className="lawyer-rec-text-box"
         onClick={() => {
@@ -19,8 +28,6 @@ function CaseLawyerRec({ item, onClick, setLawyerId }) {
         <div className="lawyer-rec-desc">{item.description}</div>
         <div className="lawyer-rec-phone">{item.phone_number}</div>
       </div>
-
-      {/* <LawyerDetail /> */}
     </div>
   );
 }
